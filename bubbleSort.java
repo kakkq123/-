011b483a7d48161bea7abbb03c779b311c5cdf55
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-04-11 00:25:55 +0900

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class bubbleSort {
+
+	public static void main(String[] args) {
+		int [] array = {8,5,6,2,4};
+		int i,j,temp;
+		int n=array.length; //배열의 길이
+		//sort
+		for(i=n-1;i>0;i--) 
+			for(j=0;j<i;j++) 
+				if(array[j]>array[j+1]) {
+					temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
+			
+				}
+			
+		
+		System.out.println("****result****");
+		for(i=0; i<n; i++)
+			System.out.printf("%d ", array[i]);
+
+	}
+
+}
