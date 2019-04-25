a19eb8d52a5fa98726672e6f68db119b563e4971
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-04-25 19:23:03 +0900

    Add files via upload

---
+++ traMatrix.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+public class traMatrix {
+	/*
+	 * 전치 행렬 만들기
+	 */
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("행의 크기를 입력하세요: ");
+		int row = kb.nextInt();
+		System.out.println("열의 크기를 입력하세요: ");
+		int col = kb.nextInt();
+		int temp;
+		int[][] a = new int[row][col];
+		int[][] at = new int[row][col];
+
+		// 입력
+		System.out.println("차례대로 원소값을 입력하세요");
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				a[i][j] = kb.nextInt();
+			}
+		}
+		System.out.println("\n*************결과*************\n");
+		// 전치행렬 출력~
+		for (int i = 0; i < row; i++) {
+			for (int j = 0; j < col; j++) {
+				at[i][j] = a[j][i];
+				System.out.printf("%d ", at[i][j]);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
