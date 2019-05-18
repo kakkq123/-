760bed3d7e4ca7c26b342296e1dbfe201bdc232c
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-05-18 23:14:49 +0900

    Add files via upload

---
+++ baek1152.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class baek1152 {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		String str = kb.nextLine();
+		String[] s = str.split("");
+
+		int i, count = 0;
+
+		for (i = 0; i < s.length; i++) {
+			if (s[i].equals(" ")) {
+				if (i == 0)
+					continue;
+				count++;
+			}
+		}
+
+		if(s[s.length-1].equals(" "))
+			count--;
+		count++;
+		System.out.printf("%d", count);
+
+	}
+
+}
