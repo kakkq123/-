68c7b28a8eab59065d3f6c431077305d20a10bb5
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-05-30 22:55:46 +0900

    Update brackets.java

--- brackets.java
+++ brackets.java
@@ -1,40 +1,43 @@
 import java.util.Scanner;
+
 public class brackets {
-	public static char[] dp= new char[10000];
-	public static int top=0;
-	public static boolean isError=false;
-	
+	public static char[] dp = new char[10000];
+	public static int top = 0;
+	public static boolean isError = false;
+
 	public static void insertBracket(char c) {
-		if(top==10000)
-			isError=true;
-		dp[top]=c;
-		top++;
+		if (top == 10000)
+			isError = true;
+		else {
+			dp[top] = c;
+			top++;
+		}
 	}
-	
-	public static void deleteBracket(char c) {
-		if(top==0)
-			isError=true;
-		top--;
+
+	public static void deleteBracket() {
+		if (top == 0)
+			isError = true;
+		else
+			top--;
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		String s=kb.nextLine();
-		char[] carray=s.toCharArray();
-		
-		for(int i=0; i<carray.length; i++) {
-			if(isError)
+		String s = kb.nextLine();
+		char[] carray = s.toCharArray();
+
+		for (int i = 0; i < carray.length; i++) {
+			if (isError)
 				break;
-			if(carray[i]=='(')
+			if (carray[i] == '(')
 				insertBracket(carray[i]);
-			else if(carray[i]==')')
-				deleteBracket(carray[i]);				
+			else if (carray[i] == ')')
+				deleteBracket();
 		}
-		if(top==0 && isError==false)
+		if (top == 0 && isError == false)
 			System.out.println("T");
 		else
 			System.out.println("F");
-		
 
 	}
 
