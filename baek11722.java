87166d4bd1fd7da98d6b25ce906f4472d4fdda1b
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-06-24 23:18:14 +0900

    Create baek11722.java

---
+++ baek11722.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class beak11722 {
+
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] a = new int[n];
+		int[] dp = new int[n];
+		int i, j, max = 1;
+
+		for (i = 0; i < n; i++) {
+			a[i] = kb.nextInt();
+			dp[i] = 1;
+		}
+
+		for (i = 0; i < n; i++) {
+			for (j = 0; j <i; j++) {
+				if (a[j] > a[i] && dp[j]+1>dp[i]) {
+					dp[i]=dp[j]+1;
+					
+				}
+			}
+			if (dp[i] > max)
+				max = dp[i];
+		}
+
+		System.out.printf("%d", max);
+	}
+
+}
