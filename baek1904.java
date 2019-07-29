414c69f7b16136277c6d7e03737d30a9c3ffb69e
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-07-29 23:38:58 +0900

    Add files via upload

---
+++ baek1904.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class baek1904 {
+	static int[] dp=new int[1000001];
+	public static int tile(int n) {
+		if(n==1)
+			return dp[1]=1;
+		if(n==2)
+			return dp[2]=2;
+		if(dp[n]!=0)
+			return dp[n]%15746;
+		return dp[n]=(tile(n-1)+tile(n-2))%15746;
+	}
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n=kb.nextInt();
+		System.out.println(tile(n));
+	}
+
+}
