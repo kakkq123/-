a890b0f5caa8c2ad5935a3651dfe3d1f784c5ca1
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-08-29 23:14:52 +0900

    Rename baek1463.java to _1463.java

---
+++ _1463.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class baek1463 {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		int n = kb.nextInt();
+		int[] dp = new int[1000002];
+
+		dp[1] = 0;
+
+		for (int i = 2; i <= n; i++) {
+			dp[i] = dp[i - 1] + 1;
+			if (i % 2 == 0 && i % 3 == 0)
+				dp[i] = Math.min(Math.min(dp[i], dp[i / 2] + 1), Math.min(dp[i], dp[i / 3] + 1));
+			else if (i % 2 == 0)
+				dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			else if (i % 3 == 0)
+				dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+		}
+		System.out.printf("%d", dp[n]);
+
+	}
+
+}
