63e0fa8ee37ffaa802589c5df8cf1b6ab7dc6de6
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-09-09 23:04:55 +0900

    Update _14501.java

--- 백준/_14501.java
+++ 백준/_14501.java
@@ -5,39 +5,39 @@ import java.util.StringTokenizer;
 
 public class _14501 {
 
- static int[] t;
- static int[] p;
- static int n, max = 0;
+	static int[] t;
+	static int[] p;
+	static int n, max = 0;
 
- public static int dfs(int day, int res) {
-  if (day > n)
-   return max = Math.max(max, res);
-  
-  if (day + t[day] <= n+1) 
-   dfs(day + t[day], res + p[day]);
+	public static int dfs(int day, int res) {
+		if (day > n)
+			return max = Math.max(max, res);
+		
+		if (day + t[day] <= n+1) 
+			dfs(day + t[day], res + p[day]);
 
-  return dfs(day + 1, res);
+		return dfs(day + 1, res);
 
- }
+	}
 
- public static void main(String[] args) throws Exception {
-  BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-  StringTokenizer s;
-  s = new StringTokenizer(br.readLine());
-  n = Integer.parseInt(s.nextToken());
-  t = new int[n + 1];
-  p = new int[n + 1];
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer s;
+		s = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(s.nextToken());
+		t = new int[n + 1];
+		p = new int[n + 1];
 
-  for (int i = 1; i <= n; i++) {
-   s = new StringTokenizer(br.readLine());
-   t[i] = Integer.parseInt(s.nextToken());
-   p[i] = Integer.parseInt(s.nextToken());
+		for (int i = 1; i <= n; i++) {
+			s = new StringTokenizer(br.readLine());
+			t[i] = Integer.parseInt(s.nextToken());
+			p[i] = Integer.parseInt(s.nextToken());
 
-  }
+		}
 
-  System.out.println(dfs(1, 0));
+		System.out.println(dfs(1, 0));
 
-  br.close();
- }
+		br.close();
+	}
 
 }
