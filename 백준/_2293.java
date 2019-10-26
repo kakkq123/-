61018db93743e4eaa58b67e5b3cf00afdcc29421
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-10-26 22:56:49 +0900

    Create _2293.java

---
+++ 백준/_2293.java
@@ -0,0 +1,25 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class _2293 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[] coin = new int[n];
+		int[] dp = new int[k + 1];
+		for (int i = 0; i < n; i++)
+			coin[i] = Integer.parseInt(br.readLine());
+		dp[0] = 1;
+		for (int i = 0; i < n; i++)
+			for (int j = 1; j <= k; j++)
+				if (j - coin[i] >= 0)
+					dp[j] += dp[j - coin[i]];
+
+		System.out.println(dp[k]);
+		br.close();
+	}
+
+}
