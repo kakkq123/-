3a27d442fcbf378a46dfdb844f1fb584ba4bf731
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2019-11-16 23:50:40 +0900

    Create _1026.java

---
+++ 백준/_1026.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _1026 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		int[] a = new int[n];
+		int[] b = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			a[i] = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++)
+			b[i] = Integer.parseInt(st.nextToken());
+
+		Arrays.sort(a);
+		Arrays.sort(b);
+
+		int result = 0;
+		for (int i = 0; i < n; i++) {
+			result += a[i] * b[n - i - 1];
+		}
+		System.out.println(result);
+		br.close();
+
+	}
+
+}
