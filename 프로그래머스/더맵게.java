6acf72609cd5ff87b593e267babff4293c0e5f96
kakkq123 <36657954+kakkq123@users.noreply.github.com>
2021-08-25 23:32:12 +0900

    Update 더맵게.java

--- 프로그래머스/더맵게.java
+++ 프로그래머스/더맵게.java
@@ -1,19 +1,20 @@
 import java.util.PriorityQueue;
+
 class Solution {
-    public int solution(int[] scoville, int K) {
-        int answer = 0;
+	public int solution(int[] scoville, int K) {
+		int answer = 0;
 		PriorityQueue<Integer> q = new PriorityQueue<Integer>();
 		for (int i = 0; i < scoville.length; i++) {
 			q.add(scoville[i]);
 		}
 		int scov = 0;
-			while (true) {
+		while (true) {
 			int a = q.poll();
-            // 모든 음식의 스코빌 지수를 K 이상
-			if(a >= K)
+			// 모든 음식의 스코빌 지수를 K 이상
+			if (a >= K)
 				return answer;
-            // 새로운 음식의 스코빌 지수를 만들지 못하면
-			if(q.size() < 1)
+			// 새로운 음식의 스코빌 지수를 만들지 못하면
+			if (q.size() < 1)
 				break;
 			int b = q.poll();
 			scov = a + b * 2;
@@ -21,5 +22,5 @@ class Solution {
 			answer++;
 		}
 		return -1;
-    }
+	}
 }
